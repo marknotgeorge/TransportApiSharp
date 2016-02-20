8e8b17dc31b53130717f26b790f404dc473ab410
Mark Johnson <marknotgeorge90@hotmail.com>
2016-02-20 21:23:36 +0000

    Replaced Nuget class library wth plain old class library. Added unit test project.

---
+++ TransportApiUnitTests/ApiCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportAPISharpUnitTests
+{
+    public static class ApiCredentials
+    {
+        public static string appId = "XXXXXXXXXX";
+        public static string appKey = "YYYYYYYYYYYYYYYYYYYYYY";
+    }
+}
---
+++ TransportApiUnitTests/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Threading.Tasks;
+using TransportAPISharp;
+
+namespace TransportAPISharpUnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestBusStopsNear()
+        {
+            var client = new TransportApiClient(ApiCredentials.apiKey, ApiCredentials.apiSecret);
+
+            var response = client.BusStopsNear(51.4728, -0.4876).Result;
+
+            Assert.AreEqual(1918, response.total);
+        }
+    }
+}
