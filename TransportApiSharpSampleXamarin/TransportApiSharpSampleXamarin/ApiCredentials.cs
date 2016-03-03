8ec51a8110d37921767057c779e985e838a469c2
Mark Johnson <marknotgeorge90@hotmail.com>
2016-03-03 23:02:26 +0000

    Added blank APICredentials file to Android sample

---
+++ TransportApiSharpSampleXamarin/TransportApiSharpSampleXamarin/ApiCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportAPISharpSample
+{
+    public static class ApiCredentials
+    {
+        public static string appId = "a3260006";
+        public static string appKey = "b3925b6db34d4ef4b98c05b4ea5881d5";
+    }
+}
