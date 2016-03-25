58dfd122f69c1668a16aa25c745b6355001144d8
Mark Johnson <marknotgeorge90@hotmail.com>
2016-03-25 23:53:58 +0000

    Sorted ApiCredentials

--- TransportApiSharpSample/ApiCredentials.cs
+++ TransportApiSharpSample/ApiCredentials.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TransportApiSharpSample
+namespace TransportAPISharp
 {
     public static class ApiCredentials
     {
-        public static string appId = "a3260006";
-        public static string appKey = "b3925b6db34d4ef4b98c05b4ea5881d5";
+        public static string appId = "xxxxxxxxxx";
+        public static string appKey = "yyyyyyyyyyyyyyyyyyyyyyy";
     }
 }
